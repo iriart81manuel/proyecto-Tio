5b04f5c2b7da5078d6d90f84b84b9e6c09ba78c2
Iriart <iriartmanuel81@gmail.com>
2024-06-22 23:10:31 -0300

    Añadimos una base de datos para trabajar en esta

---
+++ db_casino2.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-06-2024 a las 05:40:31
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_tiendaderopa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ropa`
+--
+
+CREATE TABLE `ropa` (
+  `id_ropa` int(11) NOT NULL,
+  `tipo` varchar(250) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `precio` double(10,2) NOT NULL,
+  `talle` int(11) NOT NULL,
+  `id_tienda` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
